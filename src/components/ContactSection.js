764dbf7721c2b84ef7239edbe038ebbfb0d4dd98
Stackbit Code Editor <projects@stackbit.com>
2021-06-07 10:58:53 +0000

    ContactSection.js: changed

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -8,7 +8,7 @@ export default class ContactSection extends React.Component {
         let section = _.get(this.props, 'section', null);
         return (
             <section className="content__row content__row--full-width contact__section" data-id={_.get(section, 'section_id', null)}>
-              {_.get(section, 'image', null) && (<img src={withPrefix(_.get(section, 'image', null))} alt="" className="contact__image"/>)}
+              {_.get(section, 'image', null) && (<iframe src={https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12055.780872352312!2d-74.13687730329026!3d40.9388678056402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2fb794b3dd2ad%3A0xa62e9978c8bf5d71!2sFair%20Lawn%2C%20NJ%2007410!5e0!3m2!1sen!2sus!4v1623063322428!5m2!1sen!2sus} width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" className="contact__image"/>)}
               <div className="contact__text-container">
                 <h3 className="contact__title">Have any questions? <br/> Contact us.</h3>
                 <div className="contact__info-container">
